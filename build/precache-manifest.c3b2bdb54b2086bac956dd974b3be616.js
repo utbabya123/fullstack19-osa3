78f17eb2c0e53c9f026601a06ffe9c80881e4845
unknown <jarkko.hulkko@japo.fi>
2019-03-27 17:24:43 +0200

    frontend

---
+++ build/precache-manifest.c3b2bdb54b2086bac956dd974b3be616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "922717ff94ce297db3f0",
+    "url": "/static/js/main.922717ff.chunk.js"
+  },
+  {
+    "revision": "72d8be8f669b32aec957",
+    "url": "/static/js/2.72d8be8f.chunk.js"
+  },
+  {
+    "revision": "922717ff94ce297db3f0",
+    "url": "/static/css/main.f759a539.chunk.css"
+  },
+  {
+    "revision": "a51d1596c5a79a87f4829c518662458e",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
